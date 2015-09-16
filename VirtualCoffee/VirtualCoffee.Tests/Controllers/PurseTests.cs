ff1dc7c5ba8483385bf02f76d6eede69e2ab37a8
ZorinIvanA <zorin.ivan@outlook.com>
2015-09-16 17:06:15 +0300

    И чуть-чуть тестов

---
+++ VirtualCoffee/VirtualCoffee.Tests/Controllers/PurseTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using VirtualCoffee.Repository;
+
+namespace VirtualCoffee.Tests.Controllers
+{
+    public class PurseForTests : PurseBase
+    {
+
+    }
+
+    [TestClass]
+    public class PurseTests
+    {
+        [TestMethod]
+        public void TestPurseSumShouldReturn27()
+        {
+            PurseForTests purse = new PurseForTests();
+            purse.Coins.Add(new Coin() { Value = "10", Count = 1 });
+            purse.Coins.Add(new Coin() { Value = "5", Count = 2 });
+            purse.Coins.Add(new Coin() { Value = "2", Count = 3 });
+            purse.Coins.Add(new Coin() { Value = "1", Count = 1 });
+
+            Assert.AreEqual(27, purse.Sum);
+        }
+    }
+}
